465d0dacf95df75b97de843c3331b873c5d7cc82
lily Sally <dp1@lilyyaya.top>
2025-06-30 15:29:37 +0800

    layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import { ThemeProvider } from '../components/ThemeProvider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: ' lilyTV',
+  title: ' LilyTV',
   description: '影视聚合',
   manifest: '/manifest.json',
 };
